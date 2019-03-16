363352cdf94fa7b1399991a053cc79125a67f42d
Petros Tsavdaridis <peteropon@gmail.com>
2019-03-16 21:11:37 +0100

    Add POST method

--- src/main/java/com/petros/springclient/ClientController.java
+++ src/main/java/com/petros/springclient/ClientController.java
@@ -3,15 +3,17 @@ package com.petros.springclient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @RestController
 public class ClientController {
 
     public static final Logger log = LoggerFactory.getLogger(SpringclientApplication.class);
+    String URL = "http://petros.us-east-2.elasticbeanstalk.com";
 
     private final
     RestTemplate restTemplate;
@@ -24,8 +26,7 @@ public class ClientController {
     @GetMapping(path = "/animals")
     public String getAllAnimals(){
         log.info("Request for all animals.");
-        String url = "http://localhost:8088/animals";
-        ResponseEntity<String> re = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+        ResponseEntity<String> re = restTemplate.exchange(URL+"/animals", HttpMethod.GET, null, String.class);
         return re.getBody();
     }
 
@@ -33,15 +34,16 @@ public class ClientController {
     @GetMapping(path = "/randomanimal")
     public String getRandomAnimal(){
         log.info("Request for a random animal.");
-        String url = "http://localhost:8088/randomanimal";
-        ResponseEntity<String> re = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+        ResponseEntity<String> re = restTemplate.exchange(URL+"/randomanimal", HttpMethod.GET, null, String.class);
         return re.getBody();
     }
 
     @PostMapping("/animals")
-    public ResponseEntity<String> createAnimal(@RequestBody Animal animal){
-        String url = "http://localhost:8088/animals";
-        return null;
+    public String createAnimal(@RequestBody Animal animal){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Animal> entity = new HttpEntity<>(animal, headers);
+        return restTemplate.exchange(URL+"/animals", HttpMethod.POST, entity, String.class).getBody();
     }
 
 
